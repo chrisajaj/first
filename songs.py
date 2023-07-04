250a2d76b2cd472fb9bb70a94591be1d431b17c7
chrisajaj <133458809+chrisajaj@users.noreply.github.com>
2023-07-04 03:59:34 +0100

    Create songs.py

---
+++ songs.py
@@ -0,0 +1,7 @@
+# pip install lyricsgenius
+import lyricsgenius
+api_key = "xxxxxxxxxxxxxxxxxxxxx"
+genius = lyricsgenius.Genius(api_key)
+artist = genius.search_artist("Pop Smoke", max_songs=5,sort="title")
+song = artist.song("100k On a Coupe")
+print(song.lyrics)
